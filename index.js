aa53e2e5f4724be847958a8acad526aec4560e2c
Madyson Ward <MadysonWard94@gmail.com>
2017-10-02 11:06:56 -0400

    Version 1.0.1

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 //WRITEABLE STREAMS -- allow node js to write data to a stream
 //READABLE STREAMS -- allow node to read data from a stream
 //DUPLEX --can read and write to a stream
-var fs = require("fs");
-var readStream = fs.createReadStream(__dirname + '/readfile.txt', 'utf8');
+//--const fs = require("fs");
+//--const readStream = fs.createReadStream(__dirname + "/readfile.txt", "utf8");
+//*************ADD THIS to do a write stream:***************/
+//--const writeStream = fs.createWriteStream(__dirname + "/writefile.txt");
+//--const readFile = fs.readFile("./src/index.js");
 
-//*************add this to do a write stream***************/
-var writeStream = fs.createWriteStream(__dirname + '/writefile.txt')
-//Going to pass the data in chunks
-//There's an event with createReadStream that allows us to listen to data
-//every time file passes a chunk of data on, we listen
 
-//we're splitting the writefile file to receive the data more quickly
-readStream.on('data', function(chunk){
-	console.log('new chunk received:');
-	//every time we get a chunk, we're going to write to a writestream.txt
-	writeStream.write(chunk);
+//Going to pass the data in chunks. There's an event with createReadStream 
+//that allows us to listen to data. Every time file passes a chunk of data on, 
+//we listen. We're splitting the writefile file to receive the data more quickly
+//---readStream.on("data", function(chunk){
+//--	console.log("new chunk received:");
+	//Every time we get a chunk, we're going to write to a writestream.txt
+//--	writeStream.write(chunk);
+//--});
+
+
+//DECLARE VARIABLES:
+const fs = require("fs");
+
+//READ DIRECTORIES:
+fs.readdir("./src/", function(err, files){
+	if (err){
+		return console.error(err);
+	}
+	files.forEach(function(file){
+		console.log(file);
+	});
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
