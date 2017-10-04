0bc8644d530cb87528331a1f3cd1587392597c51
Madyson Ward <MadysonWard94@gmail.com>
2017-10-04 17:07:54 -0400

    Added readme and license. Added to npmignore

--- mkfiles.test.js
+++ mkfiles.test.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+
+//STEP #1: Declare Variables
 const fs = require("fs");
 const fse = require("fs-extra");
 const readStream = fs.createReadStream(__dirname + "/readfile.js", "utf8");
@@ -11,3 +13,20 @@ readStream.on("data", function(chunk){
 	//Every time we get a chunk, we're going to write to a writestream.txt
 	writeStream.write(chunk);
 });
+
+//STEP #2: Open File
+//#2: Read file 
+//#3: Create write file
+//#4: Write file
+//#5: Truncate 
+//#6: Read again
+//#7: Close
+
+//BELOW WORKS! BUT BOTH FILES ARE CREATED AND CONTENTS ARE NOT VISIBLE TO USER.
+
+// fse.copy("./madgenerator/src/app.js", "./src/app1.js", err => {
+// 	if (err){
+// 		return console.error(err);
+// 	}
+// 	console.log("File copy created!");
+// });
