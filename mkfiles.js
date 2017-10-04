292f76834cd735d94e3a8b4122820e644a2ae461
Madyson Ward <MadysonWard94@gmail.com>
2017-10-04 16:09:11 -0400

    madgen-files copies/generates files, but contents do not transfer over with. Dirs and sub-dirs can be created with madgen-dirs.

--- mkfiles.js
+++ mkfiles.js
@@ -4,23 +4,31 @@
 /* COPY FILE & CONTENTS */
 
 //STEP #1: Declare Variables
-//const fs = require("fs"); <-- using fs-extra npm package in place for now
 const fs = require("fs");
 const fse = require("fs-extra");
+const readStream = fs.createReadStream(__dirname + "/readfile.js", "utf8");
+const writeStream = fs.createWriteStream(__dirname + "/writefile.js");
 
-//STEP #2: Open File
-fse.copy("./madgenerator/src/app.js", "./src/app1.js", err => {
-	if (err){
-		return console.error(err);
-	}
-	console.log("File copy created!");
+
+readStream.on("data", function(chunk){
+	console.log("new chunk received:");
+	//Every time we get a chunk, we're going to write to a writestream.txt
+	writeStream.write(chunk);
 });
 
-
-
-
+//STEP #2: Open File
 //#2: Read file 
 //#3: Create write file
 //#4: Write file
 //#5: Truncate 
 //#6: Read again
+//#7: Close
+
+//BELOW WORKS! BUT BOTH FILES ARE CREATED AND CONTENTS ARE NOT VISIBLE TO USER.
+
+// fse.copy("./madgenerator/src/app.js", "./src/app1.js", err => {
+// 	if (err){
+// 		return console.error(err);
+// 	}
+// 	console.log("File copy created!");
+// });
---
+++ readfile.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDom from "react-dom";
+import reduxThunk from "redux-thunk";
+import {Provider} from "react-redux";
+import {createStore, applyMiddleware} from "redux";
+import {Router, Route, IndexRoute, browserHistory} from "react-router";
+
+import App from "./components/app";
+import Signin from "./components/auth/signin";
+import Signout from "./components/auth/signout";
+import Signup from "./components/auth/signup";
+import RequireAuth from "./components/auth/require_auth";
+import ListItem from "./components/list/new-list-item";
+import ListShow from "./components/list/list-items";
+import reducers from "./reducers";
+
+
+var createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+
+ReactDom.render(
+	<Provider store={createStoreWithMiddleware(reducers)}>
+		<Router history={browserHistory}>
+			<Route path="/" component={App}>
+			<Route path="signin" component={Signin} />
+			<Route path="signout" component={Signout} />
+			<Route path="signup" component={Signup} />
+			<Route path="newitem" component={RequireAuth(ListItem)} />
+			<Route path="items" component={RequireAuth(ListShow)} />
+			</Route>
+		</Router>
+	</Provider>
+	, document.querySelector(".container")
+);
